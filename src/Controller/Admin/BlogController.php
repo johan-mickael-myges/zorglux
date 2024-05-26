2b70e747e46838caa9e5d01fc6e2e6bdec533261
johan-mickael-myges <jrakotoniaina2@myges.fr>
2024-05-26 23:01:49 +0200

    Add admin command

--- src/Controller/Admin/BlogController.php
+++ src/Controller/Admin/BlogController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Blog;
 use App\Form\BlogType;
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/blog')]
+#[Route('/admin/blog', name: 'admin_blog_')]
 class BlogController extends AbstractController
 {
-    #[Route('/', name: 'app_blog_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(BlogRepository $blogRepository): Response
     {
         return $this->render('blog/index.html.twig', [
@@ -22,7 +22,7 @@ class BlogController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_blog_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $blog = new Blog();
@@ -33,7 +33,7 @@ class BlogController extends AbstractController
             $entityManager->persist($blog);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin_blog_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('blog/new.html.twig', [
@@ -42,7 +42,7 @@ class BlogController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_blog_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Blog $blog): Response
     {
         return $this->render('blog/show.html.twig', [
@@ -50,7 +50,7 @@ class BlogController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_blog_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Blog $blog, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(BlogType::class, $blog);
@@ -59,7 +59,7 @@ class BlogController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin_blog_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('blog/edit.html.twig', [
@@ -68,7 +68,7 @@ class BlogController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_blog_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Blog $blog, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$blog->getId(), $request->request->get('_token'))) {
@@ -76,6 +76,6 @@ class BlogController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('admin_blog_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -17,18 +17,31 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        $this->addAdminUser($manager);
+
         $faker = Factory::create();
 
         // Create users
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
             $user->setUsername($faker->userName);
-            $user->setPassword($this->passwordEncoder->hashPassword($user, 'J0h@n!!'));
+            $user->setPassword($this->passwordEncoder->hashPassword($user, 'password'));
             $user->setRoles(['ROLE_USER']);
 
             $manager->persist($user);
         }
 
+        $manager->flush();
+    }
+
+    public function addAdminUser(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setUsername('mjohan');
+        $user->setPassword($this->passwordEncoder->hashPassword($user, 'J0h@n!!'));
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
