2276dde84a47963a21976431c2082ab2e418c943
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-05-29 20:47:53 +0200

    Add error pages

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
