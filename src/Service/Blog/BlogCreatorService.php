c8b2170287966c47a43d0b29e443ca2a60ed15dd
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-05-31 23:29:51 +0200

    Remove tailwind base

--- src/Service/Blog/BlogCreatorService.php
+++ src/Service/Blog/BlogCreatorService.php
@@ -25,7 +25,7 @@ class BlogCreatorService
     {
         $blog->setAuthor($this->security->getUser());
 
-        $blog->setContent($this->htmlSanitizer->sanitize($blog->getContent()));
+        $blog->setContent($blog->getContent());
         $blog->setSlug((new AsciiSlugger())->slug(strtolower($blog->getTitle())) . '-' . uniqid());
 
         $blog->setCreatedAt(new \DateTimeImmutable());
