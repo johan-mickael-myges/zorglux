e9f285a4daf9123ef18c0404987a5b09309215d1
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-06-09 00:26:12 +0200

    Add anchor tag

--- assets/controllers/clickable_controller.js
+++ assets/controllers/clickable_controller.js
@@ -5,13 +5,30 @@ export default class extends Controller {
   static values = {
     url: String,
     target: String,
+    title: String,
   }
 
   connect() {
-    // this.elementsTarget.addEventListener("click", this.click.bind(this));
+    // Append hidden anchor tag after this.element to allow robots to crawl the link for SEO
+    const anchor = document.createElement("a");
+    anchor.href = this.urlValue;
+    anchor.style.display = "none";
+    anchor.rel = "noopener noreferrer";
+
+    if (this.hasTitleValue) {
+      anchor.title = this.titleValue;
+      anchor.textContent = this.titleValue;
+      anchor.setAttribute("aria-label", this.titleValue); // Enhance accessibility
+    } else {
+      anchor.textContent = "Visit the page";
+      anchor.setAttribute("aria-label", "Visit the page");
+    }
+
+    this.element.insertAdjacentElement('afterend', anchor);
   }
 
-  visit() {
+  visit(event) {
+    event.preventDefault();
     if (this.urlValue) {
       Turbo.visit(this.urlValue)
     }
