6a75ec9ecc5240a8c4b8a8883af17bd1ae2c95a9
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-06-23 21:14:12 +0200

    subdomains in jsonld

--- src/Controller/RssFeedController.php
+++ src/Controller/RssFeedController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Blog;
 use App\Service\Blog\BlogRepositoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class RssFeedController extends AbstractController
 {
     #[Route('', name: 'rss_feed')]
-    public function rssFeed(BlogRepositoryService $service): Response
+    public function rssFeed(BlogRepositoryService $service, RequestStack $requestStack): Response
     {
         /**
          * @var Blog[] $posts
@@ -24,13 +25,13 @@ class RssFeedController extends AbstractController
 
         $channel = $rssFeed->addChild('channel');
         $channel->addChild('title', 'Zorglux');
-        $channel->addChild('link', 'https://www.zorglux.works');
+        $channel->addChild('link', $requestStack->getCurrentRequest()->getSchemeAndHttpHost());
         $channel->addChild('description', 'Découvrez les derniers articles sur Zorglux, une plateforme dynamique dédiée aux blogueurs et aux créateurs de contenu. Lisez, écrivez et partagez vos idées avec une communauté passionnée.');
 
         foreach ($posts as $post) {
             $item = $channel->addChild('item');
             $item->addChild('title', $post->getTitle());
-            $item->addChild('link', 'https://www.zorglux.works'.$this->generateUrl('blog_read', ['slug' => $post->getSlug()]));
+            $item->addChild('link', $requestStack->getCurrentRequest()->getSchemeAndHttpHost().$this->generateUrl('blog_read', ['slug' => $post->getSlug()]));
             $item->addChild('description', $post->getDescription());
             $item->addChild('pubDate', $post->getCreatedAt()->format('r'));
         }
